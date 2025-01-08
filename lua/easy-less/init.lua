55e1758dd7d2dd73ea024c311f12d9d63a8c6429
OdysseasKr <odysseaskr@users.noreply.github.com>
2025-01-08 19:46:52 +0100

    Add setup arg for command line arguments

--- lua/easy-less/init.lua
+++ lua/easy-less/init.lua
@@ -15,6 +15,7 @@ local M = {}
 local default_config = {
     show_error_message = false,
     generate_suffix = "css",
+    cmd_args = "",
 }
 
 local command = "lessc"
@@ -63,6 +64,7 @@ function M.setup(conf)
                 less_path,
                 table.concat(vim.tbl_flatten({ css_path, css_file }), "/"),
                 "--no-color",
+                default_config.cmd_args,
             }, {
                 on_stdout = callback,
                 on_stderr = callback,
