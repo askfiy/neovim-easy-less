b44951d71807e5bc43e46f4c71354a9f5a3fc0d3
Odysseas (Ody) Krystalakos <OdysseasKr@users.noreply.github.com>
2025-08-20 12:38:53 +0200

    Fallback to legacy unpack

--- lua/easy-less/init.lua
+++ lua/easy-less/init.lua
@@ -58,13 +58,13 @@ function M.setup(conf)
                 vim.fn.expand("%:t:r"),
                 default_config.generate_suffix
             )
-
+            local unpack = table.unpack or unpack
             vim.fn.jobstart({
                 command,
                 less_path,
                 table.concat(vim.tbl_flatten({ css_path, css_file }), "/"),
                 "--no-color",
-                table.unpack(default_config.cmd_args),
+                unpack(default_config.cmd_args),
             }, {
                 on_stdout = callback,
                 on_stderr = callback,
